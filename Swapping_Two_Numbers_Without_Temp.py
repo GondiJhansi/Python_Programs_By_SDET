40b45ed6946239c3bf7088e7e9f67176f6530066
Jhansi Lakshmi <84505647+GondiJhansi@users.noreply.github.com>
2022-07-26 22:02:39 +0530

    Add files via upload

---
+++ Swapping_Two_Numbers_Without_Temp.py
@@ -0,0 +1,7 @@
+num1=input("Enter 1st number:")
+num2=input("Enter 2nd number:")
+print("Value of num1 before swapping",num1)
+print("Value of num2 before swapping",num2)
+num1,num2=num2,num1
+print("Value of num1 after swapping",num1)
+print("Value of num1 after swapping",num2)
